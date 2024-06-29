4cb15a791d88c8548f871880b2c425de67ed9ac3
sumit <sumitkajla47@gmail.com>
2024-06-29 20:50:04 +0530

    done all tasks

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,11 @@
 package com.driver;
 
 public class Main {
+    public static void main(String args[]){
+        Product p = new Product();
 
+        p.product(2,3);
+        p.product(2,3,4);
+        p.product(2.0,3.0);
+    }
 }
